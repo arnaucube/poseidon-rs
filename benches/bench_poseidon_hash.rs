4c5dadf3f715312924198ab74296efca135c6a0d
arnaucube <root@arnaucube.com>
2020-11-04 19:00:40 +0100

    Use imported Fr
    v0.0.7

--- benches/bench_poseidon_hash.rs
+++ benches/bench_poseidon_hash.rs
@@ -1,22 +1,21 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
-extern crate rand;
 #[macro_use]
 extern crate ff;
 use ff::*;
 
-use poseidon_rs::Poseidon;
+use poseidon_rs::{Fr, Poseidon};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let b1: poseidon_rs::Fr = poseidon_rs::Fr::from_str(
+    let b1: Fr = Fr::from_str(
         "12242166908188651009877250812424843524687801523336557272219921456462821518061",
     )
     .unwrap();
-    let b2: poseidon_rs::Fr = poseidon_rs::Fr::from_str(
+    let b2: Fr = Fr::from_str(
         "12242166908188651009877250812424843524687801523336557272219921456462821518061",
     )
     .unwrap();
-    let mut big_arr: Vec<poseidon_rs::Fr> = Vec::new();
+    let mut big_arr: Vec<Fr> = Vec::new();
     big_arr.push(b1.clone());
     big_arr.push(b2.clone());
     let poseidon = Poseidon::new();
